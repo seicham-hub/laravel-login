e331e3a033ee2a33457314a40bfd398ad9e45b1e
seicham <onigirimentai422@gmail.com>
2021-03-29 15:34:37 +0900

    create login/logout

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginFormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+	/**
+	 * @return view
+	 */
+    public function showLogin()
+   {
+   		return view('login.login_form');
+   }
+
+
+   /**
+    * @param App\Http\Requests\LoginFormRequest
+    */
+   public function login(LoginFormRequest $request)
+   {
+   		$credentials = $request->only('email','password');
+
+   		if(Auth::attempt($credentials)){
+   			$request->session()->regenerate();
+   			return redirect()->route('home')->with('success','ログイン成功しました！');
+   		}
+   		return back()->withErrors([
+   			'danger' => 'メールアドレスかパスワードが間違っています',
+   		]);
+   }
+
+   /**
+	* ユーザーをアプリケーションからログアウトさせる
+	*
+	* @param  \Illuminate\Http\Request $request
+	* @return \Illuminate\Http\Response
+	*/
+   public function logout(Request $request)
+	{
+	    Auth::logout();
+
+	    $request->session()->invalidate();
+
+	    $request->session()->regenerateToken();
+
+	    return redirect()->route('showLogin')->with('danger','ログアウトしました！');
+	}
+
+
+}
